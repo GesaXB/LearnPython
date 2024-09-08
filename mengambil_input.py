e1310e3faca032b3ad7433ebe2b6f503f83984d2
GesaLutfen <147803544+GesaXB@users.noreply.github.com>
2024-09-08 15:19:27 +0700

    Add files via upload

---
+++ mengambil_input.py
@@ -0,0 +1,13 @@
+# latihan input user
+
+# data yang di input string
+data = input('Masukan  data: ')
+print('data = ', data, 'type = ', type(data))
+
+# data yang di input interger
+data_int = int(input('Masukan angka: '))
+print('angka = ', data_int, 'type = ', type(data_int))
+
+# boolean 
+data_bool = bool(int(input('Masukan data boolean: ')))
+print('data = ', data_bool, 'type = ', type(data_bool))
